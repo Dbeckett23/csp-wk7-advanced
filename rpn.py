09b096ff6b5f5fe012e411f1e36a01846c0d40fa
Dustin <dustinbe@umich.edu>
2019-03-10 19:39:34 -0400

    Added color modifications to output.

--- rpn.py
+++ rpn.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 #comment for travis test again
 import operator
-
+import colorama
+from colorama import init, Fore, Back, Style
+init()
 
 operators = {
     '+': operator.add,
@@ -13,6 +15,7 @@ operators = {
 
 def calculate(myarg):
     stack = list()
+    operator = ''
     for token in myarg.split():
         try:
             token = int(token)
@@ -20,6 +23,7 @@ def calculate(myarg):
         except ValueError:
             try:
                 function = operators[token]
+                operator = token
             except LookupError:
                 print("Invalid operator given.")
                 return
@@ -27,7 +31,8 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
+            print("Input: ", arg1, arg2, Fore.BLUE, operator)
+            print(Style.RESET_ALL)
     if len(stack) != 1:
         raise TypeError
     return stack.pop()
@@ -36,14 +41,19 @@ def main():
     while True:
         try:
             result = calculate(input("rpn calc> "))
-            print("Result: ", result)
+            if int(result) < 0:
+                print("Result: ", Fore.RED, result)
+                print(Style.RESET_ALL)
+            else:
+                print("Result: ", result)
+            
         except ValueError:
             print("That was not a valid input. Please try again...")
         except TypeError:
             print("Too many parameters.")
-        except:
-            print("Something crazy happened. Closing the program.")
-            quit()
+        #except:
+            #print("Something crazy happened. Closing the program.")
+         #   quit()
 
 
 if __name__ == '__main__':
